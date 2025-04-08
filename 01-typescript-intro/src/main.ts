9c5ea218d0fd57e254e6f4a12b8cfd5d317d51e1
FranRDev <franciscorodriguezgar@gmail.com>
2025-04-08 10:16:38 +0200

    Clase 13. Objetos, arreglos e interfaces

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,5 +1,6 @@
 import './style.css';
 // import './temas/01-tipos-basicos'
+import './temas/02-objeto-interfaz'
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 app.innerHTML = `Hola mundo`;
---
+++ 01-typescript-intro/src/temas/02-objeto-interfaz.ts
@@ -0,0 +1,19 @@
+const habilidades: string[] = ['Bash','Counter','Healing'];
+
+interface Personaje {
+    nombre: string;
+    puntosDeVida: number;
+    habilidades: string[];
+    puebloNatal?: string;
+}
+
+const fran: Personaje = {
+    nombre: 'Fran',
+    puntosDeVida: 100,
+    habilidades: ['Desarrollar','Ver buen cine']
+}
+
+fran.puebloNatal = 'Alcalá';
+console.table(fran)
+
+export {};
