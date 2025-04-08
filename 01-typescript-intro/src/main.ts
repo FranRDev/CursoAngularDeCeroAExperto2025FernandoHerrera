70848addc53e23a0b988b29bd6ee4490068a83e2
FranRDev <franciscorodriguezgar@gmail.com>
2025-04-08 09:14:29 +0200

    Clase 12. Tipos básicos y conceptos generales

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -1,4 +1,5 @@
-import './style.css'
+import './style.css';
+// import './temas/01-tipos-basicos'
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 app.innerHTML = `Hola mundo`;
---
+++ 01-typescript-intro/src/temas/01-tipos-basicos.ts
@@ -0,0 +1,9 @@
+const nombre: string = 'Fran';
+let puntosDeVida: number | 'FULL' = 95;
+const estaVivo: boolean = true;
+
+puntosDeVida = 'FULL';
+
+console.log({nombre, puntosDeVida, estaVivo});
+
+export {};
