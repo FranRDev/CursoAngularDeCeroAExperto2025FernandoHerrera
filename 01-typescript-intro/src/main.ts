b8d2a077ac13ab2f259f07b1dd4fe7fce67c6835
FranRDev <franciscorodriguezgar@gmail.com>
2025-04-09 11:54:47 +0200

    Clase 17. Desestructuración de Objetos

--- 01-typescript-intro/src/main.ts
+++ 01-typescript-intro/src/main.ts
@@ -2,7 +2,8 @@ import './style.css';
 // import './temas/01-tipos-basicos';
 // import './temas/02-objeto-interfaz';
 // import './temas/03-funciones';
-import './temas/04-actividad-tipos';
+// import './temas/04-actividad-tipos';
+import './temas/05-desectructuracion-basica';
 
 const app = document.querySelector<HTMLDivElement>('#app')!;
 app.innerHTML = `Hola mundo`;
---
+++ 01-typescript-intro/src/temas/05-desectructuracion-basica.ts
@@ -0,0 +1,31 @@
+interface ReproductorAudio {
+    volumenAudio: number;
+    duracionCancion: number;
+    cancion: string;
+    detalles: Detalles;
+}
+
+interface Detalles {
+    autor: string;
+    anho: number;
+}
+
+const reproductorAudio: ReproductorAudio = {
+    volumenAudio: 90,
+    duracionCancion: 36,
+    cancion: "Mess",
+    detalles: {
+        autor: 'Ed Sheeran',
+        anho: 2015
+    }
+}
+
+const cancion = 'Nueva canción';
+const { cancion: otraCancion, duracionCancion: duracion, detalles } = reproductorAudio
+const { autor } = detalles;
+
+console.log('Canción: ', otraCancion);
+console.log('Duración: ', duracion);
+console.log('Autor: ', autor);
+
+export { };
