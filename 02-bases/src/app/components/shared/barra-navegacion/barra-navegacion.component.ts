93849aa76da3344b4d875e61177d78fda05c654e
FranRDev <franciscorodriguezgar@gmail.com>
2025-04-14 08:45:38 +0200

    Clase 49. RouterLink Active

--- 02-bases/src/app/components/shared/barra-navegacion/barra-navegacion.component.ts
+++ 02-bases/src/app/components/shared/barra-navegacion/barra-navegacion.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-barra-navegacion',
-  imports: [RouterLink],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './barra-navegacion.component.html'
 })
 export class BarraNavegacionComponent {
