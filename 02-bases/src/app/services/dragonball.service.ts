ddcf29f6c79fc55e04447c8619984c9c4f4e53ff
FranRDev <franciscorodriguezgar@gmail.com>
2025-04-14 11:51:16 +0200

    Clase 61. Efectos y LocalStorage

--- 02-bases/src/app/services/dragonball.service.ts
+++ 02-bases/src/app/services/dragonball.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, signal } from '@angular/core';
+import { effect, Injectable, signal } from '@angular/core';
 
 import { Personaje } from '../interfaces/personaje.interface';
 
@@ -9,6 +9,11 @@ export class DragonBallService {
     { id: 1, nombre: 'Goku', poder: 9001 },
     { id: 2, nombre: 'Vegeta', poder: 8000 }
   ]);
+
+  guardarEnLocalStorage = effect(() => {
+    localStorage.setItem('personajes', JSON.stringify(this.personajes()));
+    console.log(`Cantidad personajes: ${this.personajes().length}`);
+  });
 
   anhadirPersonaje(personaje: Personaje) {
     this.personajes.update((lista) => [...lista, personaje]);
