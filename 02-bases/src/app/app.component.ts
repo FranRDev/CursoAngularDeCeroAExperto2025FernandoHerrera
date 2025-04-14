9c17b0a21888aced52a03cac962fbbfed43091c9
FranRDev <franciscorodriguezgar@gmail.com>
2025-04-14 08:40:04 +0200

    Clase 48. RouterLink

--- 02-bases/src/app/app.component.ts
+++ 02-bases/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { BarraNavegacionComponent } from "./components/shared/barra-navegacion/barra-navegacion.component";
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BarraNavegacionComponent],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
