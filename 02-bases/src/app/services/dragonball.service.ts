8215d76e80e9eda51bc09f8a829044cec9c2b124
FranRDev <franciscorodriguezgar@gmail.com>
2025-04-14 11:56:09 +0200

    Clase 62. LinkedSignal - Cargar del LocalStorage

--- 02-bases/src/app/services/dragonball.service.ts
+++ 02-bases/src/app/services/dragonball.service.ts
@@ -2,13 +2,15 @@ import { effect, Injectable, signal } from '@angular/core';
 
 import { Personaje } from '../interfaces/personaje.interface';
 
+const cargarDeLocalStorage = (): Personaje[] => {
+  const personajes = localStorage.getItem('personajes');
+  return personajes ? JSON.parse(personajes) : [];
+}
+
 @Injectable({ providedIn: 'root' })
 export class DragonBallService {
 
-  personajes = signal<Personaje[]>([
-    { id: 1, nombre: 'Goku', poder: 9001 },
-    { id: 2, nombre: 'Vegeta', poder: 8000 }
-  ]);
+  personajes = signal<Personaje[]>(cargarDeLocalStorage());
 
   guardarEnLocalStorage = effect(() => {
     localStorage.setItem('personajes', JSON.stringify(this.personajes()));
