c2a43e3f9e3597bb1b13e77041348ab03ed41be8
FranRDev <franciscorodriguezgar@gmail.com>
2025-04-14 09:07:16 +0200

    Clase 51. @for - Realizar iteraciones de elementos

--- 02-bases/src/app/pages/dragonball/pagina-dragonball.component.ts
+++ 02-bases/src/app/pages/dragonball/pagina-dragonball.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+interface Personaje {
+  id: number;
+  nombre: string;
+  poder: number;
+}
 
 @Component({
   imports: [],
   templateUrl: './pagina-dragonball.component.html',
 })
 export class PaginaDragonBallComponent {
+
+  personajes = signal<Personaje[]>([
+    { id: 1, nombre: 'Goku', poder: 9001 },
+    { id: 2, nombre: 'Vegeta', poder: 8000 },
+    { id: 3, nombre: 'Piccolo', poder: 3000 }
+  ]);
 
 }
