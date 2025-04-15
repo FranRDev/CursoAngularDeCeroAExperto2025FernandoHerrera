d9dd0dd2ac7c2edc3cf70b95406a758f066ffe48
FranRDev <franciscorodriguezgar@gmail.com>
2025-04-15 07:52:38 +0200

    Clase 72. RouterLinks - Desde componentes hijos

--- 03-gifs-app/src/app/gifs/components/menu-lateral-opciones/menu-lateral-opciones.component.ts
+++ 03-gifs-app/src/app/gifs/components/menu-lateral-opciones/menu-lateral-opciones.component.ts
@@ -1,7 +1,33 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface OpcionMenu {
+  icono: string;
+  ruta: string;
+  subtitulo: string;
+  titulo: string;
+}
 
 @Component({
   selector: 'gifs-menu-lateral-opciones',
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './menu-lateral-opciones.component.html'
 })
-export class MenuLateralOpcionesComponent { }
+export class MenuLateralOpcionesComponent {
+
+  opcionesMenu: OpcionMenu[] = [
+    {
+      icono: 'fa-solid fa-chart-line',
+      titulo: 'Tendencias',
+      subtitulo: 'Gifs populares',
+      ruta: '/panel/tendencias'
+    },
+    {
+      icono: 'fa-solid fa-magnifying-glass',
+      titulo: 'Buscador',
+      subtitulo: 'Buscar gifs',
+      ruta: '/panel/busqueda'
+    }
+  ]
+
+}
