e4596f29d0c24266038abd161885d7510eff72bb
FranRDev <franciscorodriguezgar@gmail.com>
2025-04-16 10:26:52 +0200

    Clase 108. Layout Components

---
+++ 04-paises-app/src/app/paises/layouts/layout-paises/layout-paises.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'layout-paises',
+  imports: [RouterOutlet],
+  templateUrl: './layout-paises.component.html',
+})
+export class LayoutPaisesComponent { }
--- 04-paises-app/src/app/paises/paises.routes.ts
+++ 04-paises-app/src/app/paises/paises.routes.ts
@@ -1,11 +1,22 @@
 import { Routes } from "@angular/router";
 
 import { PaginaPorCapitalComponent } from "./pages/por-capital/pagina-por-capital.component";
+import { LayoutPaisesComponent } from "./layouts/layout-paises/layout-paises.component";
 
 export const PaisesRoutes: Routes = [
   {
     path: '',
-    component: PaginaPorCapitalComponent
+    component: LayoutPaisesComponent,
+    children: [
+      {
+        path: 'por-capital',
+        component: PaginaPorCapitalComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'por-capital'
+      }
+    ]
   }
 ];
 
