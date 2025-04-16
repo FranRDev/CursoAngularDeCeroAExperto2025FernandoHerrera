c735a2604f583835a4536d17afdb5bc89df3aeda
FranRDev <franciscorodriguezgar@gmail.com>
2025-04-16 11:14:21 +0200

    Clase 110. Menú superior e íconos

---
+++ 04-paises-app/src/app/paises/components/menu-superior/menu-superior.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'menu-superior',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './menu-superior.component.html',
+})
+export class MenuSuperiorComponent { }
--- 04-paises-app/src/app/paises/layouts/layout-paises/layout-paises.component.ts
+++ 04-paises-app/src/app/paises/layouts/layout-paises/layout-paises.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuSuperiorComponent } from "../../components/menu-superior/menu-superior.component";
 
 @Component({
   selector: 'layout-paises',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuSuperiorComponent],
   templateUrl: './layout-paises.component.html',
 })
 export class LayoutPaisesComponent { }
