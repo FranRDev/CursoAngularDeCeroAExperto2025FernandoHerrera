c7cd7b03469a9ff28c8b06678234921892762d9c
FranRDev <franciscorodriguezgar@gmail.com>
2025-05-05 09:08:39 +0200

    Clase 138. Caché de resultados

--- 04-paises-app/src/app/paises/services/paises.service.ts
+++ 04-paises-app/src/app/paises/services/paises.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
-import { catchError, delay, map, throwError } from 'rxjs';
+import { catchError, delay, map, of, tap, throwError } from 'rxjs';
 
 import type { ElementoRestCountries } from '../interfaces/rest-countries.interfaces';
+import type { Pais } from '../interfaces/paises.interfaces';
 import { PaisesMapper } from '../mapping/paises.mapper';
 
 const API_URL = 'https://restcountries.com/v3.1';
@@ -12,14 +13,22 @@ const API_URL = 'https://restcountries.com/v3.1';
 export class PaisesService {
 
   private clienteHttp = inject(HttpClient);
+  private cacheConsultaCapital = new Map<string, Pais[]>();
 
   buscarPorCapital(texto: string) {
     texto = texto.toLocaleLowerCase();
+
+    if (this.cacheConsultaCapital.has(texto)) {
+      return of(this.cacheConsultaCapital.get(texto) ?? []);
+    }
+
+    console.log(`Consultando API por: ${texto}`);
 
     return this.clienteHttp
       .get<ElementoRestCountries[]>(`${API_URL}/capital/${texto}`)
       .pipe(
         map((elementos) => PaisesMapper.mapearElementosRestCountriesAPaises(elementos)),
+        tap(paises => this.cacheConsultaCapital.set(texto, paises)),
         catchError(error => {
           console.log('Error: ', error);
           return throwError(() => new Error(`No se encontraron países cuya capital contenga: ${texto}`));
