d385ddfa932f188e6e878e77dd3e7b422507ed93
FranRDev <franciscorodriguezgar@gmail.com>
2025-05-05 08:57:41 +0200

    Clase 137. Debounce - Búsquedas automáticas

--- 04-paises-app/src/app/paises/components/entrada-busqueda/entrada-busqueda.component.ts
+++ 04-paises-app/src/app/paises/components/entrada-busqueda/entrada-busqueda.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, output } from '@angular/core';
+import { Component, effect, input, output, signal } from '@angular/core';
 
 @Component({
   selector: 'paises-entrada-busqueda',
@@ -9,5 +9,15 @@ export class EntradaBusquedaComponent {
 
   placeholder = input<string>('Buscar');
   valor = output<string>();
+  valorEntrada = signal<string>('');
+  tiempoRebote = input<number>(300);
+
+  efectoRebote = effect((limpieza) => {
+    const valor = this.valorEntrada();
+
+    const fueraDeTiempo = setTimeout(() => this.valor.emit(valor), this.tiempoRebote());
+
+    limpieza(() => clearTimeout(fueraDeTiempo));
+  });
 
 }
