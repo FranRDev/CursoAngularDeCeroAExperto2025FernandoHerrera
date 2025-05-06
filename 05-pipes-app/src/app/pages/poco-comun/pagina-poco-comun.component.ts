2cfce4f4ac2932c6fbcc2dc05408491c6324177b
FranRDev <franciscorodriguezgar@gmail.com>
2025-05-06 12:06:15 +0200

    Clase 157. I18nSelectPipe

--- 05-pipes-app/src/app/pages/poco-comun/pagina-poco-comun.component.ts
+++ 05-pipes-app/src/app/pages/poco-comun/pagina-poco-comun.component.ts
@@ -1,11 +1,45 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 import { TarjetaComponent } from "../../components/tarjeta/tarjeta.component";
+import { I18nSelectPipe } from '@angular/common';
+
+const cliente1 = {
+  nombre: 'Fran',
+  genero: 'masculino',
+  edad: 29,
+  direccion: 'Alcalá de Guadaíra, España'
+}
+
+const cliente2 = {
+  nombre: 'Raquel',
+  genero: 'femenino',
+  edad: 25,
+  direccion: 'Carmona, España'
+}
 
 @Component({
   selector: 'pagina-poco-comun',
-  imports: [TarjetaComponent],
+  imports: [TarjetaComponent, I18nSelectPipe],
   templateUrl: './pagina-poco-comun.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PaginaPocoComunComponent { }
+export default class PaginaPocoComunComponent {
+
+  // i18n Select
+  cliente = signal(cliente1);
+
+  mapaInvitacion = {
+    masculino: 'invitarlo',
+    femenino: 'invitarla'
+  };
+
+  cambiarCliente() {
+    if (this.cliente() === cliente1) {
+      this.cliente.set(cliente2);
+      return;
+    }
+
+    this.cliente.set(cliente1);
+  }
+
+}
