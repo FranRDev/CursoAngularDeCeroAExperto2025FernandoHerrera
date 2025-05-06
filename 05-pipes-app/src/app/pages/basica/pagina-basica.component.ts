603b215a2ea94fc45b414deb31cabe6e1e02f3d1
FranRDev <franciscorodriguezgar@gmail.com>
2025-05-06 10:51:07 +0200

    Clase 153. Date Pipe

--- 05-pipes-app/src/app/pages/basica/pagina-basica.component.ts
+++ 05-pipes-app/src/app/pages/basica/pagina-basica.component.ts
@@ -1,9 +1,9 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import { LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, effect, signal } from '@angular/core';
+import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'pagina-basica',
-  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe],
+  imports: [LowerCasePipe, UpperCasePipe, TitleCasePipe, DatePipe],
   templateUrl: './pagina-basica.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -12,5 +12,18 @@ export default class PaginaBasicaComponent {
   nombreMinusculas = signal('fran');
   nombreMayusculas = signal('FRAN');
   nombreCompleto = signal('fRaN RODríGuEz');
+  fechaPersonalizada = signal(new Date());
+
+  efectoFecha = effect((limpieza) => {
+    const intervalo = setInterval(() => {
+      this.fechaPersonalizada.set(new Date());
+      console.log('Efecto ejecutado');
+    }, 1000);
+
+    limpieza(() => {
+      clearInterval(intervalo);
+      console.log('Efecto limpiado');
+    });
+  });
 
 }
