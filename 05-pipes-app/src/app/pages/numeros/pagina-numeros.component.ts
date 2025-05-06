650596b31ad4ad01d18fa69d33f2890feea304cc
FranRDev <franciscorodriguezgar@gmail.com>
2025-05-06 10:37:40 +0200

    Clase 152. Decimal, Percent y Currency Pipes

--- 05-pipes-app/src/app/pages/numeros/pagina-numeros.component.ts
+++ 05-pipes-app/src/app/pages/numeros/pagina-numeros.component.ts
@@ -1,9 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { CurrencyPipe, DecimalPipe, PercentPipe } from '@angular/common';
 
 @Component({
   selector: 'pagina-numeros',
-  imports: [],
+  imports: [DecimalPipe, PercentPipe, CurrencyPipe],
   templateUrl: './pagina-numeros.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export default class PaginaNumerosComponent { }
+export default class PaginaNumerosComponent {
+
+  totalVentas = signal(2_433_232.5567);
+  procentaje = signal(0.4856);
+
+}
