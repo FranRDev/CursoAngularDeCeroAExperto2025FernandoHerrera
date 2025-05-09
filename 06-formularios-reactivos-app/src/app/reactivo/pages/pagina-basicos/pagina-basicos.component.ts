65d93122c6de53f6142ef790685558b640065efc
FranRDev <franciscorodriguezgar@gmail.com>
2025-05-09 12:08:30 +0200

    Clase 181. ReactiveForms - FormGroup y FormControl

--- 06-formularios-reactivos-app/src/app/reactivo/pages/pagina-basicos/pagina-basicos.component.ts
+++ 06-formularios-reactivos-app/src/app/reactivo/pages/pagina-basicos/pagina-basicos.component.ts
@@ -1,9 +1,21 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
 
 @Component({
-  imports: [JsonPipe],
+  imports: [
+    ReactiveFormsModule,
+    JsonPipe
+  ],
   templateUrl: './pagina-basicos.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PaginaBasicosComponent { }
+export class PaginaBasicosComponent {
+
+  formulario = new FormGroup({
+    nombre: new FormControl(''),
+    precio: new FormControl(0),
+    existencias: new FormControl(0)
+  });
+
+}
