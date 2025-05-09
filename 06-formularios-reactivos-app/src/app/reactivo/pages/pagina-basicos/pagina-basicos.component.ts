4a66d8f389097da9005ec2ec352b6f4badae62bc
FranRDev <franciscorodriguezgar@gmail.com>
2025-05-09 12:29:49 +0200

    Clase 184. Mostrar errores en pantalla

--- 06-formularios-reactivos-app/src/app/reactivo/pages/pagina-basicos/pagina-basicos.component.ts
+++ 06-formularios-reactivos-app/src/app/reactivo/pages/pagina-basicos/pagina-basicos.component.ts
@@ -19,10 +19,35 @@ export class PaginaBasicosComponent {
   // });
 
   fb = inject(FormBuilder);
-  formulario = this.fb.group({
+  formulario: FormGroup = this.fb.group({
     nombre: ['', [Validators.required, Validators.minLength(3)]],
     precio: [0, [Validators.required, Validators.min(10)]],
     existencias: [0, [Validators.required, Validators.min(0)]]
   });
+
+  esValido(campo: string): boolean | null {
+    return !!this.formulario.controls[campo].errors;
+  }
+
+  obtenerErrorCampo(campo: string): string | null {
+    if (!this.formulario.controls[campo]) return null;
+
+    const errores = this.formulario.controls[campo].errors ?? {};
+
+    for (const key of Object.keys(errores)) {
+      switch (key) {
+        case 'required':
+          return 'Campo requerido';
+
+        case 'minlength':
+          return `Mínimo ${errores['minlength'].requiredLength} caracteres`;
+
+        case 'min':
+          return `Mínimo ${errores['min'].min}`;
+      }
+    }
+
+    return null;
+  }
 
 }
