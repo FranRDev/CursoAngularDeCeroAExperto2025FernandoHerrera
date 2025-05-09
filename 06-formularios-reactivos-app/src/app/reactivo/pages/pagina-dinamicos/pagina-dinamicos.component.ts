94e20764871000ea41e9a2eedd22a10c865cbdbd
FranRDev <franciscorodriguezgar@gmail.com>
2025-05-09 13:02:26 +0200

    Clase 187. Formularios dinámicos con arreglos

--- 06-formularios-reactivos-app/src/app/reactivo/pages/pagina-dinamicos/pagina-dinamicos.component.ts
+++ 06-formularios-reactivos-app/src/app/reactivo/pages/pagina-dinamicos/pagina-dinamicos.component.ts
@@ -1,9 +1,29 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
 
 @Component({
-  imports: [JsonPipe],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './pagina-dinamicos.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PaginaDinamicosComponent { }
+export class PaginaDinamicosComponent {
+
+  private fb = inject(FormBuilder);
+
+  formulario: FormGroup = this.fb.group({
+    nombre: ['', [Validators.required, Validators.minLength(3)]],
+    favoritos: this.fb.array(
+      [
+        ['Crash Team Racing', Validators.required],
+        ['Tombi!', Validators.required],
+      ],
+      Validators.minLength(2)
+    )
+  });
+
+  get favoritos() {
+    return this.formulario.get('favoritos') as FormArray;
+  }
+
+}
