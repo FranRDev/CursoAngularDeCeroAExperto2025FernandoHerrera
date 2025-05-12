d070088ce11a32ced2df440dfd9b3b3548aa0a0b
FranRDev <franciscorodriguezgar@gmail.com>
2025-05-12 13:01:21 +0200

    Clase 203. Primer selector controlado - Continentes

--- 06-formularios-reactivos-app/src/app/pais/pages/pagina-pais/pagina-pais.component.ts
+++ 06-formularios-reactivos-app/src/app/pais/pages/pagina-pais/pagina-pais.component.ts
@@ -1,8 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
+
+import { Pais } from '../../interfaces/paises.interfaces';
+import { PaisesService } from '../../services/paises.service';
 
 @Component({
-  imports: [],
+  imports: [JsonPipe, ReactiveFormsModule],
   templateUrl: './pagina-pais.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PaginaPaisComponent { }
+export class PaginaPaisComponent {
+
+  fb = inject(FormBuilder);
+  servicioPaises = inject(PaisesService);
+
+  regiones = signal(this.servicioPaises.regiones);
+  paises = signal<Pais[]>([]);
+  fronteras = signal<Pais[]>([]);
+
+  formulario = this.fb.group({
+    region: ['', Validators.required],
+    pais: ['', Validators.required],
+    frontera: ['', Validators.required]
+  });
+
+}
