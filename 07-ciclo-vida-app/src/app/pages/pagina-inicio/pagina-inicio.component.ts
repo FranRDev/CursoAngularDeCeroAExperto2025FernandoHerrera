434021f52d2c29cfb6c990c6c39d79283b66fe56
FranRDev <franciscorodriguezgar@gmail.com>
2025-05-13 14:45:03 +0200

    Clase 212. Ciclo de vida de los componentes

--- 07-ciclo-vida-app/src/app/pages/pagina-inicio/pagina-inicio.component.ts
+++ 07-ciclo-vida-app/src/app/pages/pagina-inicio/pagina-inicio.component.ts
@@ -1,7 +1,45 @@
 import { Component } from '@angular/core';
 
+const log = (...mensajes: string[]) => {
+  console.log(`${mensajes[0]} %c${mensajes.slice(1).join(', ')}`, 'color: #bada55');
+}
+
 @Component({
   imports: [],
   templateUrl: './pagina-inicio.component.html'
 })
-export default class PaginaInicioComponent { }
+export default class PaginaInicioComponent {
+
+  constructor() {
+    log('Constructor');
+  }
+
+  ngOnInit() {
+    log('ngOnInit', "Runs once after Angular has initialized all the component's inputs.");
+  }
+
+  ngOnChanges() {
+    log('ngOnChanges', "Runs every time the component's inputs have changed.");
+  }
+
+  ngDoCheck() {
+    log('ngDoCheck', "Runs every time this component is checked for changes.");
+  }
+
+  ngAfterContentInit() {
+    log('ngAfterContentInit', "Runs once after the component's content has been initialized.");
+  }
+
+  ngAfterContentChecked() {
+    log('ngAfterContentChecked', "Runs every time this component content has been checked for changes.");
+  }
+
+  ngAfterViewInit() {
+    log('ngAfterViewInit', "Runs once after the component's view has been initialized.");
+  }
+
+  ngAfterViewChecked() {
+    log('ngAfterViewChecked', "Runs every time the component's view has been checked for changes.");
+  }
+
+}
