5a21d2d8c06ba7a47bfc985a4053f122be229722
FranRDev <franciscorodriguezgar@gmail.com>
2025-05-15 12:00:17 +0200

    Clase 224. Mapa a pantalla completa

--- 08-mapas-app/src/app/pages/pagina-mapa-pantalla-completa/pagina-mapa-pantalla-completa.component.ts
+++ 08-mapas-app/src/app/pages/pagina-mapa-pantalla-completa/pagina-mapa-pantalla-completa.component.ts
@@ -1,7 +1,35 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, viewChild } from '@angular/core';
+
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+import { environment } from '../../../environments/environment';
+
+mapboxgl.accessToken = environment.mapBoxToken;
 
 @Component({
   imports: [],
   templateUrl: './pagina-mapa-pantalla-completa.component.html',
+  styles: `
+    div {
+      width: 100vw;
+      height: calc(100vh - 64px);
+    }
+  `
 })
-export default class PaginaMapaPantallaCompletaComponent { }
+export default class PaginaMapaPantallaCompletaComponent implements AfterViewInit {
+
+  elementoDiv = viewChild<ElementRef>('mapa')
+
+  ngAfterViewInit(): void {
+    if (!this.elementoDiv()?.nativeElement) return;
+    const elemento = this.elementoDiv()!.nativeElement;
+
+    const map = new mapboxgl.Map({
+      container: elemento, // contenedor
+      style: 'mapbox://styles/mapbox/streets-v12', // URL estilos
+      center: [-74.5, 40], // posición inicial [lng, lat]
+      zoom: 9, // zoom inicial
+    });
+  }
+
+}
