28cda82d8009d98dcc352d6b13857f1cd8c275ab
FranRDev <franciscorodriguezgar@gmail.com>
2025-05-15 13:17:26 +0200

    Clase 228. Marcadores en el mapa

--- 08-mapas-app/src/app/pages/pagina-marcadores/pagina-marcadores.component.ts
+++ 08-mapas-app/src/app/pages/pagina-marcadores/pagina-marcadores.component.ts
@@ -1,7 +1,48 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, signal, viewChild } from '@angular/core';
+
+import mapboxgl from 'mapbox-gl';
+
+import { environment } from '../../../environments/environment';
+
+mapboxgl.accessToken = environment.mapBoxToken;
 
 @Component({
   imports: [],
   templateUrl: './pagina-marcadores.component.html',
 })
-export default class PaginaMarcadoresComponent { }
+export default class PaginaMarcadoresComponent implements AfterViewInit {
+
+  elementoDiv = viewChild<ElementRef>('mapa')
+  mapa = signal<mapboxgl.Map | null>(null);
+
+  ngAfterViewInit(): void {
+    if (!this.elementoDiv()?.nativeElement) return;
+    const elemento = this.elementoDiv()!.nativeElement;
+
+    const mapa = new mapboxgl.Map({
+      container: elemento,
+      style: 'mapbox://styles/mapbox/streets-v12',
+      center: [-122.409850, 37.793085],
+      zoom: 14
+    });
+
+    const marcador = new mapboxgl.Marker({
+      draggable: false,
+      color: '#000'
+    })
+      .setLngLat([-122.409850, 37.793085])
+      .addTo(mapa);
+
+    marcador.on('dragend', evento => {
+      console.log(evento);
+    });
+
+    this.escuchadorMapa(mapa);
+  }
+
+  private escuchadorMapa(mapa: mapboxgl.Map) {
+
+    // this.mapa.set(mapa);
+  }
+
+}
