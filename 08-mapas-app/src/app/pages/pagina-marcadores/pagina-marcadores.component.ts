e90b28e8a667c48d26581eac81dccc5e8e6dbf51
FranRDev <franciscorodriguezgar@gmail.com>
2025-05-20 08:13:46 +0200

    Clase 229. Marcadores dinámicos

--- 08-mapas-app/src/app/pages/pagina-marcadores/pagina-marcadores.component.ts
+++ 08-mapas-app/src/app/pages/pagina-marcadores/pagina-marcadores.component.ts
@@ -1,10 +1,16 @@
 import { AfterViewInit, Component, ElementRef, signal, viewChild } from '@angular/core';
 
 import mapboxgl from 'mapbox-gl';
+import { v4 as UUIDv4 } from 'uuid';
 
 import { environment } from '../../../environments/environment';
 
 mapboxgl.accessToken = environment.mapBoxToken;
+
+interface Marcador {
+  id: string;
+  marcadorMapBox: mapboxgl.Marker;
+};
 
 @Component({
   imports: [],
@@ -14,6 +20,7 @@ export default class PaginaMarcadoresComponent implements AfterViewInit {
 
   elementoDiv = viewChild<ElementRef>('mapa')
   mapa = signal<mapboxgl.Map | null>(null);
+  marcadores = signal<Marcador[]>([]);
 
   ngAfterViewInit(): void {
     if (!this.elementoDiv()?.nativeElement) return;
@@ -26,23 +33,43 @@ export default class PaginaMarcadoresComponent implements AfterViewInit {
       zoom: 14
     });
 
-    const marcador = new mapboxgl.Marker({
-      draggable: false,
-      color: '#000'
-    })
-      .setLngLat([-122.409850, 37.793085])
-      .addTo(mapa);
+    // const marcador = new mapboxgl.Marker({
+    //   draggable: false,
+    //   color: '#000'
+    // })
+    //   .setLngLat([-122.409850, 37.793085])
+    //   .addTo(mapa);
 
-    marcador.on('dragend', evento => {
-      console.log(evento);
-    });
+    // marcador.on('dragend', evento => {
+    //   console.log(evento);
+    // });
 
     this.escuchadorMapa(mapa);
   }
 
   private escuchadorMapa(mapa: mapboxgl.Map) {
+    mapa.on('click', evento => this.clicMapa(evento));
+    this.mapa.set(mapa);
+  }
 
-    // this.mapa.set(mapa);
+  clicMapa(evento: mapboxgl.MapMouseEvent) {
+    if (!this.mapa()) return;
+    const mapa = this.mapa()!;
+
+    const marcador = new mapboxgl.Marker({
+      draggable: false,
+      color: this.generarColorHexAleatorio()
+    })
+      .setLngLat(evento.lngLat)
+      .addTo(mapa);
+
+    this.marcadores.update(marcadores => [{ id: UUIDv4(), marcadorMapBox: marcador }, ...marcadores])
+
+    console.log(this.marcadores());
+  }
+
+  private generarColorHexAleatorio(): string {
+    return '#xxxxxx'.replace(/x/g, () => ((Math.random() * 16) | 0).toString(16));
   }
 
 }
