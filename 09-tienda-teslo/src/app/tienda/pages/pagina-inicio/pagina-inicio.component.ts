a9fed66519064c2286b80527987d908c1ed8b532
FranRDev <franciscorodriguezgar@gmail.com>
2025-05-20 13:48:50 +0200

    Clase 243. Tarjetas de producto y FadeIn animation

--- 09-tienda-teslo/src/app/tienda/pages/pagina-inicio/pagina-inicio.component.ts
+++ 09-tienda-teslo/src/app/tienda/pages/pagina-inicio/pagina-inicio.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { TarjetaProductoComponent } from "../../../productos/components/tarjeta-producto/tarjeta-producto.component";
 
 @Component({
-  imports: [],
+  imports: [TarjetaProductoComponent],
   templateUrl: './pagina-inicio.component.html',
 })
 export default class PaginaInicioComponent { }
