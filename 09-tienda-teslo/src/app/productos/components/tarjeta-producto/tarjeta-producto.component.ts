102886c0230c70d251c494378feac7f71a136c2d
FranRDev <franciscorodriguezgar@gmail.com>
2025-05-22 10:23:57 +0200

    Clase 249. Solución a la tarea - Mostrar Productos

--- 09-tienda-teslo/src/app/productos/components/tarjeta-producto/tarjeta-producto.component.ts
+++ 09-tienda-teslo/src/app/productos/components/tarjeta-producto/tarjeta-producto.component.ts
@@ -1,16 +1,19 @@
+import { SlicePipe } from '@angular/common';
 import { Component, computed, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Product } from '@productos/interfaces/productos.interface';
 
 @Component({
   selector: 'tarjeta-producto',
-  imports: [RouterLink],
+  imports: [RouterLink, SlicePipe],
   templateUrl: './tarjeta-producto.component.html',
 })
 export class TarjetaProductoComponent {
 
-  titulo = input.required<string>();
-  descripcion = input.required<string>();
-  nombreImagen = input.required<string>();
-  urlImagen = computed<string>(() => `http://localhost:3000/api/files/product/${this.nombreImagen()}`);
+  producto = input.required<Product>();
+  // titulo = input.required<string>();
+  // descripcion = input.required<string>();
+  // nombreImagen = input.required<string>();
+  urlImagen = computed<string>(() => `http://localhost:3000/api/files/product/${this.producto().images[0]}`);
 
 }
