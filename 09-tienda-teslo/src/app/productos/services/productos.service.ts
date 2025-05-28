7e26d59e39f7cbdfe16eda507a8d61f9161e2f8d
FranRDev <franciscorodriguezgar@gmail.com>
2025-05-28 09:09:25 +0200

    Clase 263. Caché de productos

--- 09-tienda-teslo/src/app/productos/services/productos.service.ts
+++ 09-tienda-teslo/src/app/productos/services/productos.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 
-import { Observable } from 'rxjs';
+import { Observable, of, tap } from 'rxjs';
 
 import { environment } from 'src/environments/environment';
 import { Product, ProductsResponse } from '@productos/interfaces/productos.interface';
@@ -19,9 +19,17 @@ export class ServicioProductosService {
 
   private clienteHttp = inject(HttpClient);
 
+  private cacheProductos = new Map<string, ProductsResponse>();
+
   obtenerProductos(opciones: Opciones): Observable<ProductsResponse> {
     const { limite = 9, salto = 0, genero = '' } = opciones;
-    return this.clienteHttp.get<ProductsResponse>(`${urlBase}/products`, { params: { limit: limite, offset: salto, gender: genero } });
+
+    const llave = `${limite}-${salto}-${genero}`;
+    if (this.cacheProductos.has(llave)) { return of(this.cacheProductos.get(llave)!); }
+
+    return this.clienteHttp
+      .get<ProductsResponse>(`${urlBase}/products`, { params: { limit: limite, offset: salto, gender: genero } })
+      .pipe(tap(respuesta => this.cacheProductos.set(llave, respuesta)));
   }
 
   obtenerProducto(idOSlug: string): Observable<Product> {
