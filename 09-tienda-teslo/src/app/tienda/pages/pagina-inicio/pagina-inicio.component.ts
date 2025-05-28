7824b88d47f6b71745ad7508ad5fbb3fbb762064
FranRDev <franciscorodriguezgar@gmail.com>
2025-05-28 08:30:24 +0200

    Clase 261. Paginación con RxResource

--- 09-tienda-teslo/src/app/tienda/pages/pagina-inicio/pagina-inicio.component.ts
+++ 09-tienda-teslo/src/app/tienda/pages/pagina-inicio/pagina-inicio.component.ts
@@ -1,6 +1,8 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, inject } from '@angular/core';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop'
 
-import { rxResource } from '@angular/core/rxjs-interop'
+import { map } from 'rxjs';
 
 import { PaginadorComponent } from '@shared/components/paginador/paginador.component';
 import { ServicioProductosService } from '@productos/services/productos.service';
@@ -16,11 +18,24 @@ import { TarjetaProductoComponent } from '@productos/components/tarjeta-producto
 export default class PaginaInicioComponent {
 
   servicioProductos = inject(ServicioProductosService);
+  rutaActiva = inject(ActivatedRoute);
+
+  paginaActual = toSignal(
+    this.rutaActiva.queryParamMap.pipe(
+      map(parametros => parametros.get('pagina') ? +parametros.get('pagina')! : 1),
+      map(pagina => isNaN(pagina) ? 1 : pagina)
+    ),
+    {
+      initialValue: 1
+    }
+  );
 
   recursoProductos = rxResource({
-    request: () => ({}),
-    loader: () => {
-      return this.servicioProductos.obtenerProductos({});
+    request: () => ({ pagina: this.paginaActual() - 1 }),
+    loader: ({ request }) => {
+      return this.servicioProductos.obtenerProductos({
+        salto: request.pagina * 9
+      });
     }
   });
 
