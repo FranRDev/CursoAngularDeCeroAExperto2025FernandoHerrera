474c5f0e83b7eada828107582f63c56b83dfb04e
FranRDev <franciscorodriguezgar@gmail.com>
2025-05-28 08:19:13 +0200

    Clase 260. Paginación local - Funcionalidad

--- 09-tienda-teslo/src/app/shared/components/paginador/paginador.component.ts
+++ 09-tienda-teslo/src/app/shared/components/paginador/paginador.component.ts
@@ -1,14 +1,16 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, input, linkedSignal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'paginador',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './paginador.component.html',
 })
 export class PaginadorComponent {
 
   paginas = input(0);
   paginaActual = input<number>(1);
+  paginaActiva = linkedSignal(this.paginaActual);
 
   obtenerPaginas = computed(() => Array.from({ length: this.paginas() }, (_, indice) => indice + 1));
 
