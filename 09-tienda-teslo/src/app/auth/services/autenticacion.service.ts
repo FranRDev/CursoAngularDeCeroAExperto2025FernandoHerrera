1ddb0dd012411c001ee8f66e4dbf8e778257196d
FranRDev <franciscorodriguezgar@gmail.com>
2025-05-29 13:24:18 +0200

    Clase 275. Revisar autenticación previa

--- 09-tienda-teslo/src/app/auth/services/autenticacion.service.ts
+++ 09-tienda-teslo/src/app/auth/services/autenticacion.service.ts
@@ -5,6 +5,7 @@ import { environment } from 'src/environments/environment';
 import { RespuestaAuth } from '@auth/interfaces/respuesta-auth.interfce';
 import { User } from '@auth/interfaces/usuario.interface';
 import { catchError, map, Observable, of, tap } from 'rxjs';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 type EstadoAutenticacion = 'comprobando' | 'autenticado' | 'no-autenticado';
 
@@ -19,6 +20,10 @@ export class AutenticacionService {
 
   private clienteHttp = inject(HttpClient);
 
+  recursoComprobarEstado = rxResource({
+    loader: () => this.comprobarEstado()
+  });
+
   estadoAutenticacion = computed<EstadoAutenticacion>(() => {
     if (this._estadoAutenticacion() === 'comprobando') return 'comprobando';
     if (this._usuario()) return 'autenticado';
@@ -28,9 +33,32 @@ export class AutenticacionService {
   usuario = computed(() => this._usuario());
   token = computed(() => this._token());
 
-  iniciarSesion(correo: string, clave: string) : Observable<boolean> {
+  iniciarSesion(correo: string, clave: string): Observable<boolean> {
     return this.clienteHttp
       .post<RespuestaAuth>(`${urlBase}/auth/login`, { email: correo, password: clave })
+      .pipe(
+        tap(respuesta => {
+          this._usuario.set(respuesta.user);
+          this._token.set(respuesta.token);
+          localStorage.setItem('token', respuesta.token);
+          this._estadoAutenticacion.set('autenticado');
+        }),
+        map(() => true),
+        catchError(() => {
+          this._usuario.set(null);
+          this._token.set(null);
+          this._estadoAutenticacion.set('no-autenticado');
+          return of(false);
+        })
+      );
+  }
+
+  comprobarEstado(): Observable<boolean> {
+    const token = localStorage.getItem('token');
+    if (!token) return of(false);
+
+    return this.clienteHttp
+      .get<RespuestaAuth>(`${urlBase}/auth/check-status`, { headers: { Authorization: `Bearer ${token}` } })
       .pipe(
         tap(respuesta => {
           this._usuario.set(respuesta.user);
