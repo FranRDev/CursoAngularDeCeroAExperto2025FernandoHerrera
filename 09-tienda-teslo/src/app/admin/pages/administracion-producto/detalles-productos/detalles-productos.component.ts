c2c52b8428eeef84e3c02c82ad8cd81f5b65decc
FranRDev <franciscorodriguezgar@gmail.com>
2025-06-03 08:27:02 +0200

    Clase 293. Formulario de producto

--- 09-tienda-teslo/src/app/admin/pages/administracion-producto/detalles-productos/detalles-productos.component.ts
+++ 09-tienda-teslo/src/app/admin/pages/administracion-producto/detalles-productos/detalles-productos.component.ts
@@ -1,16 +1,55 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CarruselProductoComponent } from '@productos/components/carrusel-producto/carrusel-producto.component';
 
 import { Product } from '@productos/interfaces/productos.interface';
+import { FormUtils } from '@utils/utilidades-formularios';
 
 @Component({
-  imports: [CarruselProductoComponent],
+  imports: [CarruselProductoComponent, ReactiveFormsModule],
   selector: 'detalles-productos',
   templateUrl: './detalles-productos.component.html'
 })
-export class DetallesProductosComponent {
+export class DetallesProductosComponent implements OnInit {
 
   producto = input.required<Product>();
+
+  fb = inject(FormBuilder);
+
+  formulario = this.fb.group({
+    titulo: ['', Validators.required],
+    descripcion: ['', Validators.required],
+    slug: ['', [Validators.required, Validators.pattern(FormUtils.slugPattern)]],
+    precio: [0, [Validators.required, Validators.min(0)]],
+    existencias: [0, [Validators.required, Validators.min(0)]],
+    tallas: [[]],
+    imagenes: [[]],
+    etiquetas: [''],
+    genero: ['hombre', [Validators.required, Validators.pattern(/hombre|mujer|ninhos|unisex/)]]
+  });
+
   tallas = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
+
+  ngOnInit(): void {
+    this.establecerValorFormulario(this.producto())
+  }
+
+  establecerValorFormulario(producto: Partial<Product>) {
+    this.formulario.reset({
+      titulo: producto.title,
+      descripcion: producto.description,
+      slug: producto.slug,
+      precio: producto.price,
+      existencias: producto.stock,
+      //tallas: producto.sizes,
+      //imagenes: producto.images,
+      etiquetas: producto.tags?.join(','),
+      genero: producto.gender
+    });
+  }
+
+  enviar() {
+    console.log(this.formulario.value);
+  }
 
 }
