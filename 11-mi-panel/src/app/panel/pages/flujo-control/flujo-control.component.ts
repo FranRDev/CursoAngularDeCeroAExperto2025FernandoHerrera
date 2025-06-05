c592ae2c54ffead0e5e6b584f3764dbdb6a9b8c7
FranRDev <franciscorodriguezgar@gmail.com>
2025-06-05 12:38:29 +0200

    Clase 329. Control Flow - @Switch

--- 11-mi-panel/src/app/panel/pages/flujo-control/flujo-control.component.ts
+++ 11-mi-panel/src/app/panel/pages/flujo-control/flujo-control.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
+type Grado = 'A' | 'B' | 'F';
+
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [],
@@ -9,6 +11,7 @@ import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 export default class FlujoControlComponent {
 
   public mostrarContenido = signal(false);
+  public grado = signal<Grado>('A');
 
   public conmutarContenido() {
     this.mostrarContenido.update(valor => !valor);
