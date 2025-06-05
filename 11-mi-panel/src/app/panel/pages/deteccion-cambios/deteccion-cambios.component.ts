fecb83fcbeca9eaf9dd35b1695ded3ac0787caf3
FranRDev <franciscorodriguezgar@gmail.com>
2025-06-05 13:59:47 +0200

    Clase 332. ChangeDetection - OnPush

--- 11-mi-panel/src/app/panel/pages/deteccion-cambios/deteccion-cambios.component.ts
+++ 11-mi-panel/src/app/panel/pages/deteccion-cambios/deteccion-cambios.component.ts
@@ -1,9 +1,38 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TituloComponent } from '@shared/titulo/titulo.component';
 
 @Component({
-  selector: 'app-deteccion-cambios',
-  imports: [],
-  templateUrl: './deteccion-cambios.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [TituloComponent, JsonPipe],
+  selector: 'app-deteccion-cambios',
+  template: `
+    <titulo [titulo]="frameworkActual()" />
+    <pre>{{ senhalFramework() | json }}</pre>
+    <pre>{{ propiedadFramework | json }}</pre>
+  `,
 })
-export default class DeteccionCambiosComponent { }
+export default class DeteccionCambiosComponent {
+
+  public frameworkActual = computed(() => `Detección de Cambios - ${this.senhalFramework().nombre}`);
+
+  public senhalFramework = signal({
+    nombre: 'Angular',
+    lanzamiento: 2016
+  });
+
+  public propiedadFramework = {
+    nombre: 'Angular',
+    lanzamiento: 2016
+  };
+
+  constructor() {
+    setTimeout(() => {
+      // this.propiedadFramework.nombre = 'React';
+      this.senhalFramework.update(valor => ({ ...valor, nombre: 'React' }));
+
+      console.log('Hecho');
+    }, 3000);
+  }
+
+}
