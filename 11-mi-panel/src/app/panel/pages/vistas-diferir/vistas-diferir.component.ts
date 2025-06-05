a8eba670b665bfecd9d73d20f36b9fcd47b54f0e
FranRDev <franciscorodriguezgar@gmail.com>
2025-06-05 14:17:33 +0200

    Clase334. Defer Triggers

--- 11-mi-panel/src/app/panel/pages/vistas-diferir/vistas-diferir.component.ts
+++ 11-mi-panel/src/app/panel/pages/vistas-diferir/vistas-diferir.component.ts
@@ -1,9 +1,10 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CargadoresPesadosLentoComponent } from '@shared/cargadores-pesados/cargadores-pesados-lento.component';
+import { TituloComponent } from '@shared/titulo/titulo.component';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [CargadoresPesadosLentoComponent],
+  imports: [CargadoresPesadosLentoComponent, TituloComponent],
   selector: 'vistas-diferir',
   templateUrl: './vistas-diferir.component.html'
 })
